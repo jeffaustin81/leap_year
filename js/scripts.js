434cd858ab0a661aac2f2c3bdd79a7b8fc948873
Julio Diaz & Jeff Austin <student@epicodus.com>
2015-09-08 11:22:15 -0700

    Add additional check

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 var leapYear = function(year) {
-	if ((year % 4 === 0) && (year % 100 !== 0)) {
+	if ((year % 400 === 0)) {
+		return true;
+	}
+	else if ((year % 4 === 0) && (year % 100 !== 0)) {
 		return true;
 	} else {
 		return false;
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,8 @@ describe('leapYear', function() {
 	it("is false for most years divisible by 100", function() {
 		expect(leapYear(1900)).to.equal(false);
 	});
+	
+	it("is true for years divisible by 400", function() {
+		expect(leapYear(4000)).to.equal(true);
+	});
 });
